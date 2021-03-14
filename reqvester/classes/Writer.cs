c417283a49b214d81c639b332ded3cb98bcbd637
Евгений Шайкин <sers.beck@gmail.com>
2021-03-14 21:51:06 +0300

    запись в файл.

---
+++ reqvester/classes/Writer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Reqvester
+{
+    public class Writer
+    {
+        public async Task WriteToFile(Dictionary<string, int> buffer)
+        {
+            string[] lines = new string[buffer.Count];
+            var i = 0;
+
+            foreach (var buf in buffer)
+            {
+   
+                lines[i] = buf.Key + " - " + buf.Value;
+                i++;
+            }
+
+            await File.WriteAllLinesAsync("WriteLines.txt", lines);
+        }
+    }
+}
